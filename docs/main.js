5bae2488e924a3c0789b9316b3cee832e5f3e1a9
Tune <cfdw011@gmail.com>
2021-07-31 22:07:57 +0900

    #편지 화면,오픈그래프

--- docs/main.js
+++ docs/main.js
@@ -14,6 +14,7 @@ $(document).ready(
             {
                 console.log("gdgd")
                 $("#base2").show()
+                $("#base2").removeClass("base-hide")
                 $("#base2").addClass("base-show")
                 $("#base1").addClass("base-hide-up")
             }else{
@@ -21,5 +22,14 @@ $(document).ready(
                 alert('다시 입력해주세요 :)')
             }
         })
+        $(".arrow").on("click",(e)=>{
+                console.log("gdgd")
+                $("#base3").show()
+                $("#base3").removeClass("base-hide")
+                $("#base3").addClass("base-show")
+                $("#base2").addClass("base-hide-up")
+                $("#base2").removeClass("base-show")
+        })
+        
     }
 )
